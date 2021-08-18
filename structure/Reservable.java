929704f33020a7b809dc2262bdeefb157aeaf3f0
Gabriel-T-Harris <gabrieltobanharris@hotmail.com>
2021-08-17 23:23:23 -0400

    Added Javadoc to Reservable.

--- structure/Reservable.java
+++ structure/Reservable.java
@@ -3,15 +3,25 @@ package structure;
 /**
 <b>
 Purpose: Requirement to be a node for evaluation purposes.<br>
-Programmer: Alexander Herman Oxorn <br>
-Date: 2021-07-29
+Programmer: Alexander Herman Oxorn, Gabriel Toban Harris <br>
+Date: 2021-07-29/2021-8-17
 </b>
 */
 
 public interface Reservable
 {
-    void reserve();
-    void release();
-
+    /**
+     * Checks reservedness of this card.
+     */
     boolean isReserved();
+
+    /**
+     * Reserves this card.
+     */
+    void reserve();
+
+    /**
+     * Frees this card.
+     */
+    void release();
 }
