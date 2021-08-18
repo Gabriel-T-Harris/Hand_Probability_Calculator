d30aa775de0f4024708139ead4bddf2cdc9e8118
Gabriel-T-Harris <gabrieltobanharris@hotmail.com>
2021-08-17 23:21:10 -0400

    Corrected Merge Revert Artifacts

--- structure/Binary_Operator_Node.java
+++ structure/Binary_Operator_Node.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package structure;
 
 import java.util.Arrays;
@@ -53,42 +52,3 @@ public abstract class Binary_Operator_Node<T> extends Base_Node<T>
         return Arrays.asList(this.LEFT_CHILD, this.RIGHT_CHILD);
     }
 }
-=======
-package structure;
-
-/**
-<b>
-Purpose: Base of the binary operators.<br>
-Programmer: Gabriel Toban Harris <br>
-Date: 2021-07-24
-</b>
-*/
-
-//TODO: consider converting to at least 2
-public abstract class Binary_Operator_Node<T> extends Base_Node<T>
-{
-    /**
-     * Left operand.
-     */
-    public final Evaluable<T> LEFT_CHILD;
-
-    /**
-     * Right operand.
-     */
-    public final Evaluable<T> RIGHT_CHILD;
-
-    /**
-     * Constructor.
-     * 
-     * @param NAME of the node
-     * @param LEFT_CHILD is the left operand of the operator
-     * @param RIGHT_CHILD is the right operand of the operator
-     */
-    public Binary_Operator_Node(String NAME, final Evaluable<T> LEFT_CHILD, final Evaluable<T> RIGHT_CHILD)
-    {
-        super(NAME);
-        this.LEFT_CHILD = LEFT_CHILD;
-        this.RIGHT_CHILD = RIGHT_CHILD;
-    }
-}
->>>>>>> parent of 3a3d00d... Clean Up
