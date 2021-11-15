dc4a174fc7d194bec23ee658f603a4fb3be5be3e
Gabriel-T-Harris <gabrieltobanharris@hotmail.com>
2021-11-14 23:55:16 -0500

    Added License
    Source files now have licenses. Also renamed the notes file.

--- structure/Evaluable.java
+++ structure/Evaluable.java
@@ -1,15 +1,30 @@
+/*
+    Copyright (C) 2021 Gabriel Toban Harris
+
+        This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 package structure;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringJoiner;
-import java.util.Arrays;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
 <b>
